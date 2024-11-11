82e38f6a41b9ffb0b9d7b3ed1097442e7e28f172
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-11 17:54:24 -0300

    feat: add a default title for the document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Next.js Pages Router Internationalization Example</title>
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
